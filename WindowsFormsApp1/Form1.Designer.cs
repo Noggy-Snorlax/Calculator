e94f99aae9da9792a9a99430b32655a783e9487b
Noggy Snorlax <hoong.970912@hotmail.com>
2022-11-03 00:51:14 +0800

    [just a calculator]

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,180 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.answer = new System.Windows.Forms.Label();
+            this.textBoxInput = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.PostFixLabel = new System.Windows.Forms.Label();
+            this.PostFixValue = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // answer
+            // 
+            this.answer.AutoSize = true;
+            this.answer.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.answer.Location = new System.Drawing.Point(269, 85);
+            this.answer.MinimumSize = new System.Drawing.Size(100, 0);
+            this.answer.Name = "answer";
+            this.answer.Size = new System.Drawing.Size(100, 20);
+            this.answer.TabIndex = 0;
+            // 
+            // textBoxInput
+            // 
+            this.textBoxInput.Location = new System.Drawing.Point(267, 47);
+            this.textBoxInput.Name = "textBoxInput";
+            this.textBoxInput.Size = new System.Drawing.Size(496, 26);
+            this.textBoxInput.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(267, 119);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(122, 45);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Submit";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // PostFixLabel
+            // 
+            this.PostFixLabel.AutoSize = true;
+            this.PostFixLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.PostFixLabel.Location = new System.Drawing.Point(12, 329);
+            this.PostFixLabel.Name = "PostFixLabel";
+            this.PostFixLabel.Size = new System.Drawing.Size(78, 20);
+            this.PostFixLabel.TabIndex = 3;
+            this.PostFixLabel.Text = "PostFix: ";
+            // 
+            // PostFixValue
+            // 
+            this.PostFixValue.AutoSize = true;
+            this.PostFixValue.BackColor = System.Drawing.SystemColors.Control;
+            this.PostFixValue.Location = new System.Drawing.Point(87, 329);
+            this.PostFixValue.Name = "PostFixValue";
+            this.PostFixValue.Size = new System.Drawing.Size(33, 20);
+            this.PostFixValue.TabIndex = 4;
+            this.PostFixValue.Text = "null";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 47);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(227, 20);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Input your calculation here:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(166, 85);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(73, 20);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Answer:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 309);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(82, 20);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Extra Info:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(500, 185);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(104, 20);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "How to use:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label5.Location = new System.Drawing.Point(500, 205);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(270, 160);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "1) \'+\' for addition\r\n2) \'-\' for subtration\r\n3) \'*\' for multiplication\r\n4) \'/\' for" +
+    " division\r\n5) You can use bracket too\r\n6) Input can have space\r\n\r\nExample Input " +
+    ": 10 - ( 2 + 3 * ( 7 - 5 ) )";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.PostFixValue);
+            this.Controls.Add(this.PostFixLabel);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBoxInput);
+            this.Controls.Add(this.answer);
+            this.Name = "Form1";
+            this.Text = "Calculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label answer;
+        private System.Windows.Forms.TextBox textBoxInput;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label PostFixLabel;
+        private System.Windows.Forms.Label PostFixValue;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,243 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+
+    public partial class Form1 : Form
+    {
+
+        bool isOperator(string c)
+        {
+            if (c == "+" || c == "-" || c == "*" || c == "/" || c == "^")
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        int precedence(string c)
+        {
+            if (c == "^")
+                return 3;
+            else if (c == "*" || c == "/")
+                return 2;
+            else if (c == "+" || c == "-")
+                return 1;
+            else
+                return -1;
+        }
+
+        public double simpleCalculator(double number1, double number2, string operatorSymbol)
+        {
+            switch (operatorSymbol)
+            {
+
+                case "+":
+                    return number1 + number2;
+
+                case "-":
+                    return number1 - number2;
+                case "*":
+                    return number1 * number2;
+                case "/":
+                    return number1 / number2;
+                default:
+                    Console.WriteLine("Nothing");
+                    break;
+            }
+            return 0;
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                this.answer.Text = "" + Calculate(textBoxInput.Text);
+            }catch
+            {
+                this.answer.Text = "Invalid Input or Have some bug. Sorry";
+                this.answer.ForeColor = System.Drawing.Color.Red;
+            }
+
+        }
+
+        private ArrayList PrefixList(string prefixText)
+        {
+            ArrayList ArrayListPrefix = new ArrayList();
+            string prefixTrim = prefixText.Replace(" ", "");
+            while (!prefixTrim.Equals(""))
+            {
+                int operatorIndex = prefixTrim.IndexOfAny(new char[] { '+', '-', '/', '*', '(', ')' });
+                if (operatorIndex == 0)
+                {
+                    operatorIndex = operatorIndex + 1;
+                }
+                if (operatorIndex != -1)
+                {
+                    ArrayListPrefix.Add(prefixTrim.Substring(0, operatorIndex));
+                    prefixTrim = prefixTrim.Remove(0, operatorIndex);
+                    if (!prefixTrim.Equals(""))
+                    {
+                        int operatorIndex2 = prefixTrim.IndexOfAny(new char[] { '+', '-', '/', '*', '(', ')' });
+                        if (operatorIndex2 == 0)
+                        {
+                            operatorIndex2++;
+                        }
+                        if (operatorIndex2 != -1)
+                        {
+                            ArrayListPrefix.Add(prefixTrim.Substring(0, operatorIndex2));
+                            prefixTrim = prefixTrim.Remove(0, operatorIndex2);
+                        }
+                    }
+                }
+                else
+                {
+                    ArrayListPrefix.Add(prefixTrim.Substring(0, prefixTrim.Length));
+                    prefixTrim = prefixTrim.Remove(0, prefixTrim.Length);
+                }
+            }
+
+            return ArrayListPrefix;
+        }
+
+        public string getPostFixFromPostFixArraylist(ArrayList postFixArrayList)
+        {
+            string PostFixAnswer = "";
+            foreach (Object X in postFixArrayList)
+            {
+                PostFixAnswer += X.ToString() + " ";
+            }
+
+
+            return PostFixAnswer;
+        }
+
+        public ArrayList convertPreFixIntoPostFix(ArrayList preFix)
+        {
+            ArrayList postFixList = new ArrayList();
+            Stack<string> stackOperator = new Stack<string>();
+            foreach (string item in preFix)
+            {
+                if (item.Equals("("))
+                {
+                    stackOperator.Push(item);
+                }
+                else if (item.Equals(")"))
+                {
+                    while (!stackOperator.Peek().Equals("("))
+                    {
+                        postFixList.Add(stackOperator.Pop());
+                    }
+                    stackOperator.Pop();
+                }
+                else
+                {
+                    if (isOperator(item) && stackOperator.Count == 0)
+                    {
+                        stackOperator.Push(item);
+                    }
+                    else if (isOperator(item) && stackOperator.Count != 0)
+                    {
+                        if (stackOperator.Peek().Equals("("))
+                        {
+                            stackOperator.Push(item);
+                        }
+                        else
+                        {
+                            if (precedence(item) < precedence(stackOperator.Peek()))
+                            {
+                                postFixList.Add(stackOperator.Pop());
+
+                            }
+                            if (stackOperator.Count != 0)
+                            {
+                                if (precedence(item) > precedence(stackOperator.Peek()))
+                                {
+                                    stackOperator.Push(item);
+                                }
+                                else if (precedence(item) == precedence(stackOperator.Peek()))
+                                {
+                                    postFixList.Add(stackOperator.Pop());
+                                    stackOperator.Push(item);
+                                }
+                            }
+                            else
+                            {
+                                stackOperator.Push(item);
+                            }
+                        }
+                    }
+                    else
+                    {
+                        postFixList.Add(item);
+                    }
+                }
+            }
+            foreach (string operatorItem in stackOperator)
+            {
+                postFixList.Add(operatorItem);
+            }
+            return postFixList;
+        }
+
+        public double postFixToAnswer(ArrayList Postfix)
+        {
+            ArrayList postFixToAnswerTemp = Postfix;
+
+            while (postFixToAnswerTemp.Count > 1)
+            {
+                for (int count = 0; count < postFixToAnswerTemp.Count; count++)
+                {
+                    if (isOperator("" + postFixToAnswerTemp[count]))
+                    {
+                        double number1 = Convert.ToDouble(postFixToAnswerTemp[count - 2]);
+                        double number2 = Convert.ToDouble(postFixToAnswerTemp[count - 1]);
+                        string operatorSymbol = postFixToAnswerTemp[count].ToString();
+
+                        double tempcal = simpleCalculator(number1, number2, operatorSymbol);
+                        postFixToAnswerTemp.RemoveAt(count - 2);
+                        postFixToAnswerTemp.RemoveAt(count - 2);
+                        postFixToAnswerTemp.RemoveAt(count - 2);
+                        postFixToAnswerTemp.Insert(count - 2, tempcal);
+                        break;
+                    }
+                }
+            }
+
+            return Convert.ToDouble(postFixToAnswerTemp[0]);
+        }
+
+        public double Calculate(string sum)
+        {
+            this.PostFixValue.Text = "";
+            string prefix = sum;
+            //string prefix = ("( 11.5 + 15.4 ) + 10.1");
+             
+            ArrayList arrayListPrefix = PrefixList(prefix);
+            ArrayList postFixList = convertPreFixIntoPostFix(arrayListPrefix);
+            string postFixAnswer = getPostFixFromPostFixArraylist(postFixList);
+            double postFixToAnswerDisplay = postFixToAnswer(postFixList);
+            this.PostFixValue.Text = ""+ postFixAnswer;
+
+            return Convert.ToDouble(postFixToAnswerDisplay);
+        }
+
+    }
+}
